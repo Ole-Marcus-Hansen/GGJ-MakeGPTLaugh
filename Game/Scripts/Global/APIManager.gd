5c2f30828b4c64044289a891cb44d5ca6e9804d9
OleMarcusHansen <omhanse@hiof.no>
2024-01-27 12:23:26 +0100

    fix api manager and add signal

--- Game/Scripts/Global/APIManager.gd
+++ Game/Scripts/Global/APIManager.gd
@@ -1,5 +1,7 @@
 extends Node
 
+signal completed(rating, comment)
+
 @onready var http_request = HTTPRequest.new()
 
 const url = "https://api.openai.com/v1/chat/completions"
@@ -8,19 +10,22 @@ const maxTokens = 128
 const headers = ["Content-type: application/json", "Authorization: Bearer " + Secrets.key]
 const model = "gpt-3.5-turbo"
 
-@onready var prompt = "hello, tell me a joke"
+@onready var prompt = "Rate this joke from 1 to 10. Respond in JSON format {rating: int, comment: string}. Here is the joke: "
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	add_child(http_request)
 	http_request.request_completed.connect(self._http_request_completed)
+	
+	# Test call
+	# callGPT("why did the chicken cross the road? To get across")
 
 
-
-func _callGPT():
+func callGPT(input_joke):
 	var messages = [
 		{"role": "user",
-		"content": prompt}
+		"content": prompt + input_joke}
 	]
 	
 	var body = JSON.stringify({
@@ -30,9 +35,6 @@ func _callGPT():
 		"model": model
 	})
 	
-	
-	
-	# Perform a GET request. The URL below returns JSON as of writing.
 	var error = http_request.request(url, headers, HTTPClient.METHOD_POST, body)
 	if error != OK:
 		push_error("An error occurred in the HTTP request.")
@@ -45,8 +47,11 @@ func _http_request_completed(result, response_code, headers, body):
 	var response = json.get_data()
 	
 	var message = response["choices"][0]["message"]["content"]
-
-	# Will print the user agent string used by the HTTPRequest node (as recognized by httpbin.org).
-	print(response.headers["User-Agent"])
-	
 	print("message: " + message)
+	
+	json.parse(message)
+	var message_split = json.get_data()
+	
+	print("split: " + str(message_split["rating"]) + ", " + message_split["comment"])
+	
+	completed.emit(message_split["rating"], message_split["comment"])
